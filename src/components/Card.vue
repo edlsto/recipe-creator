b114e56609de5a8f9c203c936a95798ab964aeed
Edward Stoner <edwardlstoner@gmail.com>
2020-07-25 16:35:47 -0600

    Add image

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="card">
     <router-link :to="{ name: 'recipe', params: { id: id } }">
-      {{ title }}
+      <img class="card-img" :src="image" alt="" />
+
+      <h4 class="card-title">
+        {{ title }}
+      </h4>
+      <div class="row-2">
+        <div class="serves">Serves: {{ serves }}</div>
+        <div class="serves">Time: {{ prepTime + cookTime }} minutes</div>
+      </div>
     </router-link>
   </div></template
 >
@@ -9,18 +17,35 @@
 <script>
 export default {
   name: "Card",
-  props: ["id", "title", "ingredients"],
+  props: [
+    "id",
+    "title",
+    "ingredients",
+    "image",
+    "serves",
+    "prepTime",
+    "cookTime",
+  ],
 };
 </script>
 
 <style>
 .card {
-  border: 1px solid black;
-  margin: 1em;
-  padding: 2em;
-  border-radius: 1em;
   display: flex;
-  justify-content: center;
-  align-items: center;
+}
+.card-img {
+  width: 100%;
+  border-radius: 1em;
+  height: 70%;
+}
+.card-title {
+  margin-top: 1em;
+}
+.row-2 {
+  display: flex;
+  justify-content: space-between;
+  color: gray;
+  font-size: 0.8em;
+  margin-top: 0.5em;
 }
 </style>
