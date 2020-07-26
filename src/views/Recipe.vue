7e4b5131ec89cdc4681cc304cedcb27d09a4b47d
Edward Stoner <edwardlstoner@gmail.com>
2020-07-25 20:36:03 -0600

    Add more details/style to recipe page

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,24 +1,42 @@
 <template>
   <div class="about">
-    <h1>{{ selectedRecipe.title }}</h1>
-    <h2>Ingredients</h2>
-    <ul>
-      <li
-        v-for="ingredient in selectedRecipe.ingredients"
-        :key="ingredient.key"
-      >
-        {{ ingredient }}
-      </li>
-    </ul>
-    <ol>
-      <li v-for="ingredient in selectedRecipe.steps" :key="ingredient.key">
-        {{ ingredient }}
-      </li>
-    </ol>
-    <p>Prep time: {{ selectedRecipe.prepTime }}</p>
-    <p>Cook time: {{ selectedRecipe.cookTime }}</p>
-    <p>Serves: {{ selectedRecipe.serves }}</p>
-    <img class="recipe-img" :src="selectedRecipe.image" alt="" />
+    <div class="text">
+      <h1 class="recipe-title">{{ selectedRecipe.title }}</h1>
+      <h4 class="recipe-subtitle">Ingredients</h4>
+      <ul class="recipe-list">
+        <li
+          v-for="(ingredient, index) in selectedRecipe.ingredients"
+          :key="index"
+          class="recipe-item"
+        >
+          {{ ingredient }}
+        </li>
+      </ul>
+      <h4 class="recipe-subtitle">Steps</h4>
+      <ol class="recipe-list">
+        <li
+          v-for="(step, index) in selectedRecipe.steps"
+          :key="index"
+          class="recipe-item"
+        >
+          {{ step }}
+        </li>
+      </ol>
+    </div>
+    <div class="image">
+      <img class="recipe-img" :src="selectedRecipe.image" alt="" />
+      <div class="details">
+        <div>
+          <h4 class="recipe-subtitle">
+            Prep time: {{ selectedRecipe.prepTime }} minutes
+          </h4>
+          <h4 class="recipe-subtitle">
+            Cook time: {{ selectedRecipe.cookTime }} minutes
+          </h4>
+        </div>
+        <h4 class="recipe-subtitle">Serves: {{ selectedRecipe.serves }}</h4>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,5 +55,32 @@ export default {
 <style>
 .recipe-img {
   width: 100%;
+}
+.about {
+  display: flex;
+}
+
+.text {
+  flex: 0 0 40%;
+}
+.recipe-title {
+  margin-bottom: 0.5em;
+}
+
+.recipe-subtitle {
+  margin-bottom: 1em;
+}
+.recipe-list {
+  margin-bottom: 1em;
+}
+
+.details {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1em;
+}
+
+.recipe-item {
+  margin-bottom: 0.7em;
 }
 </style>
