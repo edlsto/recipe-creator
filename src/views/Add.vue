e85feb0e557c9f24e5ac6b1b741540a11d8901d6
Edward Stoner <edwardlstoner@gmail.com>
2020-07-26 08:14:28 -0600

    Style form

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,100 +1,106 @@
 <template>
   <div class="add">
-    <div class="add-text">
-      <h1 class="recipe-title">Add a new recipe</h1>
-      <div class="add-item">
-        <label for="title">Recipe name:</label>
-        <input
-          class="add-item-input"
-          v-model="newRecipe.title"
-          type="text"
-          id="title"
-        />
-      </div>
+    <div class="two-col">
+      <div class="add-text">
+        <h1 class="recipe-title">Add a new recipe</h1>
+        <div class="add-item">
+          <label for="title">Recipe name:</label>
+          <input
+            class="add-item-input"
+            v-model="newRecipe.title"
+            type="text"
+            id="title"
+          />
+        </div>
 
-      <div class="add-item">
-        <label for="cook-time">Cook time:</label>
-        <input
-          class="add-item-input"
-          v-model="newRecipe.cookTime"
-          type="number"
-          id="cook-time"
-        />
-      </div>
-      <div class="add-item">
-        <ul v-if="newRecipe.ingredients.length">
-          <li v-for="(ingredient, index) in newRecipe.ingredients" :key="index">
-            {{ ingredient }}
-          </li>
-        </ul>
-        <div class="form-row">
-          <label for="ingredient">Ingredient:</label>
-          <div class="input-btn-container">
-            <input
-              v-model="newIngredient"
-              class="add-item-input"
-              type="text"
-              v-on:keyup.enter="onSubmitIngredient"
-              id="ingredient"
-            />
-            <button v-on:click="onSubmitIngredient" class="add-item-button">
-              Add
-            </button>
+        <div class="add-item stack">
+          <h4>Ingredients</h4>
+          <ul>
+            <li v-if="!newRecipe.ingredients.length">No ingredients added</li>
+            <li
+              v-for="(ingredient, index) in newRecipe.ingredients"
+              :key="index"
+            >
+              {{ ingredient }}
+            </li>
+          </ul>
+          <div class="form-row">
+            <label for="ingredient">New ingredient:</label>
+            <div class="input-btn-container">
+              <input
+                v-model="newIngredient"
+                class="add-item-input"
+                type="text"
+                v-on:keyup.enter="onSubmitIngredient"
+                id="ingredient"
+              />
+            </div>
           </div>
+          <button v-on:click="onSubmitIngredient" class="add-item-button">
+            Add
+          </button>
+        </div>
+        <div class="add-item stack">
+          <ul v-if="newRecipe.steps.length">
+            <li v-for="(step, index) in newRecipe.steps" :key="index">
+              {{ step }}
+            </li>
+          </ul>
+          <div class="form-row">
+            <label for="step">Step:</label>
+            <div class="input-btn-container">
+              <textarea
+                v-model="newStep"
+                type="text"
+                v-on:keyup.enter="onSubmitStep"
+                id="step"
+              />
+            </div>
+          </div>
+          <button v-on:click="onSubmitStep" class="add-item-button">
+            Add
+          </button>
+        </div>
+        <div class="add-item">
+          <label for="prep-time|">Prep time:</label>
+          <input
+            v-model="newRecipe.prepTime"
+            type="number"
+            class="add-item-input"
+            id="prep-time"
+          />
+        </div>
+        <div class="add-item">
+          <label for="cook-time">Cook time:</label>
+          <input
+            class="add-item-input"
+            v-model="newRecipe.cookTime"
+            type="number"
+            id="cook-time"
+          />
+        </div>
+        <div class="add-item">
+          <label for="serves">Serves:</label>
+          <input
+            v-model="newRecipe.serves"
+            type="number"
+            class="add-item-input"
+            id="serves"
+          />
         </div>
       </div>
-      <div class="add-item">
-        <ul v-if="newRecipe.steps.length">
-          <li v-for="(step, index) in newRecipe.steps" :key="index">
-            {{ step }}
-          </li>
-        </ul>
-        <div class="form-row">
-          <label for="step">Step:</label>
-          <div class="input-btn-container">
-            <input
-              v-model="newStep"
-              type="text"
-              v-on:keyup.enter="onSubmitStep"
-              class="add-item-input"
-              id="step"
-            />
-            <button v-on:click="onSubmitStep" class="add-item-button">
-              Add
-            </button>
-          </div>
+      <div class="add-image">
+        <div v-if="!newRecipe.image">
+          <h4>Select an image</h4>
+          <input type="file" @change="onFileChange(newRecipe, $event)" />
+        </div>
+        <div v-else>
+          <img :src="newRecipe.image" class="img-preview" />
+          <button @click="removeImage(newRecipe)">Remove image</button>
         </div>
       </div>
-      <div class="add-item">
-        <label for="prep-time|">Prep time:</label>
-        <input
-          v-model="newRecipe.prepTime"
-          type="number"
-          class="add-item-input"
-          id="prep-time"
-        />
-      </div>
-      <div class="add-item">
-        <label for="serves">Serves:</label>
-        <input
-          v-model="newRecipe.serves"
-          type="number"
-          class="add-item-input"
-          id="serves"
-        />
-      </div>
     </div>
-    <div class="add-image">
-      <div v-if="!newRecipe.image">
-        <h4>Select an image</h4>
-        <input type="file" @change="onFileChange(newRecipe, $event)" />
-      </div>
-      <div v-else>
-        <img :src="newRecipe.image" class="img-preview" />
-        <button @click="removeImage(newRecipe)">Remove image</button>
-      </div>
-      <button v-on:click="submitList">Submit</button>
-    </div>
+    <button v-on:click="submitList" class="submit">Submit</button>
   </div>
 </template>
 
@@ -152,7 +158,17 @@ export default {
 </script>
 
 <style>
+.submit {
+  align-self: center;
+}
 .add {
+  display: flex;
+  flex-direction: column;
+}
+.stack {
+  flex-direction: column;
+}
+.two-col {
   display: flex;
   justify-content: space-between;
 }
@@ -163,21 +179,25 @@ export default {
 .add-item {
   margin-bottom: 1em;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
 }
 
 .add-item-input {
-  height: 2em;
+  height: 1.7em;
   flex: 1 0 auto;
+  font-size: 1em;
+  outline: none;
 }
 .form-row {
   display: flex;
   justify-content: space-between;
   flex: 1 0 auto;
+  flex-direction: column;
 }
 
 .add-item-button {
-  margin-left: 1em;
+  margin: 1em 0 0 1em;
+  align-self: center;
 }
 
 .add-text {
@@ -191,7 +211,16 @@ export default {
   display: flex;
 }
 
+.add-image {
+  flex: 0 0 50%;
+}
+
 label {
   margin-right: 1em;
+}
+
+textarea {
+  height: 100px;
+  flex: 1 0 auto;
 }
 </style>
