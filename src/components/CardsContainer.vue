9f62114f4a410dfb767959589b3b05ad96e47fdc
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 12:59:58 -0600

    Move select to home

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,34 +1,16 @@
 <template>
-  <div class="cards-container-wrapper">
-    <div class="select-container">
-      <label for="filter-select" class="filter-label">Filter</label>
-      <select
-        name="filter"
-        id="filter-select"
-        v-model="selected"
-        v-on:change="$emit('filter', selected)"
-        class="select-filter"
-      >
-        <option value="">--Sort recipes--</option>
-        <option value="Name-Asc">By name (A to Z)</option>
-        <option value="Name-Dsc">By name (Z to A)</option>
-        <option value="Time-Asc">By cooking time (shortest to longest)</option>
-        <option value="Time-Dsc">By cooking time (longest to shortest)</option>
-      </select>
-    </div>
-    <div class="cards-container">
-      <Card
-        v-for="recipe in recipes"
-        v-bind:key="recipe.id"
-        v-bind:title="recipe.title"
-        v-bind:ingredients="recipe.ingredients"
-        v-bind:id="recipe.id"
-        v-bind:serves="recipe.serves"
-        v-bind:prepTime="recipe.prepTime"
-        v-bind:cookTime="recipe.cookTime"
-        v-bind:image="recipe.image"
-      />
-    </div>
+  <div class="cards-container">
+    <Card
+      v-for="recipe in recipes"
+      v-bind:key="recipe.id"
+      v-bind:title="recipe.title"
+      v-bind:ingredients="recipe.ingredients"
+      v-bind:id="recipe.id"
+      v-bind:serves="recipe.serves"
+      v-bind:prepTime="recipe.prepTime"
+      v-bind:cookTime="recipe.cookTime"
+      v-bind:image="recipe.image"
+    />
   </div>
 </template>
 
@@ -37,11 +19,6 @@ import Card from "./Card";
 export default {
   components: {
     Card,
-  },
-  data() {
-    return {
-      selected: "",
-    };
   },
   name: "CardsContainer",
   props: ["recipes"],
@@ -63,6 +40,7 @@ export default {
   justify-content: center;
   align-items: center;
   margin-top: 10em;
+  font-weight: 700;
 }
 
 .select-filter {
