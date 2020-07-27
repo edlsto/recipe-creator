015d5383042b1ccb5b0c1fc34647de4ed7d0dc55
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 13:00:12 -0600

    Move select to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div id="home">
-    <CardsContainer :recipes="recipes" v-on="$listeners" />
+  <div class="cards-container-wrapper" v-if="recipes.length > 0">
+    <div class="select-container">
+      <label for="filter-select" class="filter-label">Filter</label>
+      <select
+        name="filter"
+        id="filter-select"
+        v-model="selected"
+        v-on:change="$emit('filter', selected)"
+        class="select-filter"
+      >
+        <option value="">--Sort recipes--</option>
+        <option value="Name-Asc">By name (A to Z)</option>
+        <option value="Name-Dsc">By name (Z to A)</option>
+        <option value="Time-Asc">By cooking time (shortest to longest)</option>
+        <option value="Time-Dsc">By cooking time (longest to shortest)</option>
+      </select>
+    </div>
+    <CardsContainer :recipes="recipes" />
   </div>
+  <div v-else class="no-recipes">No recipes</div>
 </template>
 
 <script>
@@ -12,5 +29,10 @@ export default {
   },
   name: "Home",
   props: ["recipes"],
+  data() {
+    return {
+      selected: "",
+    };
+  },
 };
 </script>
