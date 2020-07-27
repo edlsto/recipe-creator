c8fe98516e1a24ea980b79ccb1f5418bdc4b3e86
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 15:25:56 -0600

    Remove sample data

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,6 @@
 <script>
 import Nav from "./components/Nav.vue";
 
-import TacosImg from "./assets/tacos.jpg";
-import SoupImg from "./assets/butternut-squash-soup.jpg";
-import ToastImg from "./assets/avocado-toast.jpg";
-import SPImg from "./assets/sweet-potato.png";
 export default {
   components: {
     Nav,
@@ -89,107 +85,7 @@ export default {
     return {
       searchInput: "",
       filter: "",
-      recipes: [
-        {
-          id: 1,
-          title: "Chicken Tacos",
-          ingredients: [
-            "1 pound skinless, boneless chicken breast halves, cut into bite size pieces",
-            "1 cup lemonade",
-            "2 tablespoons olive oil",
-            "1 tablespoon lime juice",
-            "1 ½ teaspoons Worcestershire sauce",
-            "½ teaspoon garlic powder",
-            "½ teaspoon onion powder",
-            "1 bay leaf",
-            "1 (12 ounce) package corn tortillas",
-            "1 head lettuce, shredded",
-            "2 large tomatoes, chopped",
-            "1 (8 ounce) package shredded sharp Cheddar cheese",
-            "1 (8 ounce) jar salsa",
-            "1 (8 ounce) container sour cream",
-          ],
-          steps: [
-            "In a large skillet over medium heat, combine chicken, lemonade, olive oil, lime juice, and Worcestershire sauce. Season with garlic powder, onion powder, and bay leaf. Simmer until chicken is no longer pink, and juices run clear, 15 to 20 minutes.",
-            "Meanwhile, warm the tortillas in the oven or microwave until soft. When chicken is fully cooked, transfer to serving bowl. Place lettuce, tomatoes, cheese, salsa, and sour cream in serving dishes. Each person can create their own wrap, using their preferred ingredients.",
-          ],
-          prepTime: 20,
-          cookTime: 20,
-          serves: 8,
-          image: TacosImg,
-        },
-        {
-          id: 2,
-          title: "Butternut Squash Soup",
-          ingredients: [
-            "6 tablespoons chopped onion",
-            "4 tablespoons margarine",
-            "6 cups peeled and cubed butternut squash",
-            "3 cups water",
-            "4 cubes chicken bouillon",
-            "½ teaspoon dried marjoram",
-            "¼ teaspoon ground black pepper",
-            "⅛ teaspoon ground cayenne pepper",
-            "2 (8 ounce) packages cream cheese",
-          ],
-          steps: [
-            "In a large saucepan, saute onions in margarine until tender. Add squash, water, bouillon, marjoram, black pepper and cayenne pepper. Bring to boil; cook 20 minutes, or until squash is tender.",
-            "Puree squash and cream cheese in a blender or food processor in batches until smooth. Return to saucepan, and heat through. Do not allow to boil.",
-          ],
-          prepTime: 25,
-          cookTime: 35,
-          serves: 6,
-          image: SoupImg,
-        },
-        {
-          id: 3,
-          title: "Avocado Toast (Vegan)",
-          ingredients: [
-            "4 slices whole-grain bread",
-            "1 avocado, halved and pitted",
-            "2 tablespoons chopped fresh parsley",
-            "1 ½ teaspoons extra-virgin olive oil",
-            "½ lemon, juiced",
-            "½ teaspoon salt",
-            "½ teaspoon ground black pepper",
-            "½ teaspoon onion powder",
-            "½ teaspoon garlic powder",
-          ],
-          steps: [
-            "Toast bread in a toaster or toaster oven.",
-            "Scoop avocado into a bowl. Add parsley, olive oil, lemon juice, salt, pepper, onion powder, and garlic powder; mash together using a potato masher. Spread avocado mixture into each piece of toast.",
-          ],
-          prepTime: 10,
-          cookTime: 10,
-          serves: 4,
-          image: ToastImg,
-        },
-        {
-          id: 4,
-          title: "Sweet Potato Casserole Dessert",
-          ingredients: [
-            "4 ½ cups cooked and mashed sweet potatoes",
-            "½ cup butter, melted",
-            "⅓ cup milk",
-            "1 cup white sugar",
-            "½ teaspoon vanilla extract  ",
-            "2 eggs, beaten",
-            "1 cup light brown sugar",
-            "½ cup all-purpose flour",
-            "⅓ cup butter",
-            "1 cup chopped pecans",
-          ],
-          steps: [
-            "Preheat oven to 350 degrees F (175 degrees C). Grease a 9x13 inch baking dish.",
-            "In a large bowl, mix together mashed sweet potatoes, 1/2 cup butter, milk, sugar, vanilla extract, and eggs. Spread sweet potato mixture into the prepared baking dish. In a small bowl, mix together brown sugar and flour. Cut in 1/3 cup butter until mixture is crumbly, then stir in pecans. Sprinkle pecan mixture over the sweet potatoes.",
-            "Bake for 25 minutes in the preheated oven, or until golden brown.",
-          ],
-          prepTime: 20,
-          cookTime: 25,
-          serves: 16,
-          image: SPImg,
-        },
-      ],
+      recipes: [],
     };
   },
 };
