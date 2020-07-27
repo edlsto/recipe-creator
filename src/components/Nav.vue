253c6689e725a64ad44375cba1de692354577794
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 15:19:57 -0600

    Move search bar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -12,14 +12,6 @@
       <router-link to="/" class="nav-link">All recipes</router-link>
 
       <router-link to="/add" class="nav-link">Add new recipe</router-link>
-
-      <input
-        type="text"
-        class="search"
-        v-model="search"
-        v-on:keyup="setSearch"
-        placeholder="Search by recipe title"
-      />
     </div>
   </div>
 </template>
@@ -27,17 +19,6 @@
 <script>
 export default {
   name: "Nav",
-  methods: {
-    setSearch: function() {
-      console.log("fired");
-      this.$emit("search-input", this.search);
-    },
-  },
-  data() {
-    return {
-      search: "",
-    };
-  },
 };
 </script>
 
@@ -49,11 +30,13 @@ export default {
   align-items: center;
   padding: 2em 5em;
 }
+
 .logo-text-container {
+  height: 100%;
   display: flex;
   align-items: center;
-  height: 100%;
 }
+
 .logo {
   height: 3em;
 }
@@ -76,19 +59,6 @@ a {
   flex-wrap: wrap;
 }
 
-.search {
-  height: 50%;
-  width: 15em;
-  margin-left: 1em;
-  border: none;
-  height: 2.5em;
-  border-radius: 0.7em;
-  background: 4% / 8% no-repeat url("../assets/search.png");
-  background-color: rgb(233, 233, 233);
-  padding-left: 2.3em;
-  font-size: 1em;
-  outline: none;
-}
 .nav-link.router-link-exact-active {
   border-bottom: 3px solid rgb(123, 216, 146);
   margin-bottom: -3px;
@@ -113,6 +83,7 @@ a {
   .nav-links {
     flex-direction: column;
   }
+
   .nav-link {
     padding: 0.5em;
   }
@@ -128,6 +99,7 @@ a {
   .main {
     margin: 0 auto 4em;
   }
+
   .nav-link.router-link-exact-active {
     border-bottom: 0;
     margin-bottom: 0;
