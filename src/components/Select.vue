2d3a02c2e472d3f8665d650040f4cd9759ab4d8b
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 13:26:44 -0600

    Create select component

---
+++ src/components/Select.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="select-container">
+    <label for="filter-select" class="filter-label">Filter</label>
+    <select
+      name="filter"
+      id="filter-select"
+      v-model="selected"
+      v-on:change="$emit('filter', selected)"
+      class="select-filter"
+    >
+      <option value="">--Sort recipes--</option>
+      <option value="Name-Asc">By name (A to Z)</option>
+      <option value="Name-Dsc">By name (Z to A)</option>
+      <option value="Time-Asc">By cooking time (shortest to longest)</option>
+      <option value="Time-Dsc">By cooking time (longest to shortest)</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Select",
+  data() {
+    return {
+      selected: "",
+    };
+  },
+};
+</script>
