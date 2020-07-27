cdad56bfada363008bb0e13226cce23d0f604d7e
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 13:26:54 -0600

    Create select component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,6 @@
 <template>
   <div class="cards-container-wrapper" v-if="recipes.length > 0">
-    <div class="select-container">
-      <label for="filter-select" class="filter-label">Filter</label>
-      <select
-        name="filter"
-        id="filter-select"
-        v-model="selected"
-        v-on:change="$emit('filter', selected)"
-        class="select-filter"
-      >
-        <option value="">--Sort recipes--</option>
-        <option value="Name-Asc">By name (A to Z)</option>
-        <option value="Name-Dsc">By name (Z to A)</option>
-        <option value="Time-Asc">By cooking time (shortest to longest)</option>
-        <option value="Time-Dsc">By cooking time (longest to shortest)</option>
-      </select>
-    </div>
+    <Select v-on="$listeners" />
     <CardsContainer :recipes="recipes" />
   </div>
   <div v-else class="no-recipes">No recipes</div>
@@ -23,16 +8,24 @@
 
 <script>
 import CardsContainer from "../components/CardsContainer";
+import Select from "../components/Select";
 export default {
   components: {
+    Select,
     CardsContainer,
   },
   name: "Home",
   props: ["recipes"],
-  data() {
-    return {
-      selected: "",
-    };
-  },
 };
 </script>
+
+<style>
+.no-recipes {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  flex: 1 0 auto;
+  margin-bottom: 5em;
+}
+</style>
