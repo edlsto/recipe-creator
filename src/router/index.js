b9c9abab36086bcdb8cb4841691b9666e12e1f64
Edward Stoner <edwardlstoner@gmail.com>
2020-07-27 07:59:29 -0600

    Add scroll behavior to router

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,7 @@ const routes = [
     path: "/recipe/:id/edit",
     name: "Edit",
     component: () => import("../views/Add.vue"),
+    props: true,
   },
 ];
 
@@ -28,6 +29,9 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
